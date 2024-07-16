362ee6821d15699f73ca89776eb1df27de6e6ba2
Wim Henderickx <wim.henderickx@gmail.com>
2024-07-16 16:40:45 +0200

    fixed condition

--- pkg/reconcilers/schema/reconciler.go
+++ pkg/reconcilers/schema/reconciler.go
@@ -228,7 +228,7 @@ func (r *reconciler) handleError(ctx context.Context, cr *invv1alpha1.Schema, ms
 		r.recorder.Eventf(cr, corev1.EventTypeWarning, crName, msg)
 		return true // recoverable error
 	} else {
-		cr.SetConditions(invv1alpha1.Failed(err.Error()))
+		cr.SetConditions(condv1alpha1.Failed(err.Error()))
 		log.Error(msg, "error", err)
 		r.recorder.Eventf(cr, corev1.EventTypeWarning, crName, fmt.Sprintf("%s, err: %s", msg, err.Error()))
 		myError, ok := err.(*myerror.MyError)
